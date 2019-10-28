ce35aec1b7451c6cfe1d212b123008b96b77dfe6
1185491736@qq.com <1185491736@qq.com>
2019-10-28 21:49:39 +0800

    歌词数据解析

--- miniprogram/components/lyric/lyric.js
+++ miniprogram/components/lyric/lyric.js
@@ -12,20 +12,40 @@ Component({
   },
   observers: {
     lyric(lrc) {
-      console.log(lrc)
+      this._parseLyric(lrc)
     }
   },
   /**
    * 组件的初始数据
    */
   data: {
-
+    lrcList: []
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    _parseLyric(sLyric) {
+      let line = sLyric.split('\n')
+      const _lrcList = []
+      line.forEach((elem) => {
+        let time = elem.match(/\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/g)
+        if(time) {
+          let lrc = elem.split(time)[1]
+          // 需要把时间转化为秒
+          let timeReg = time[0].match(/\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/)
+          let time2Seconds = parseInt(timeReg[1]) * 60 + parseInt(timeReg[2]) + parseInt(timeReg[3]) / 1000
+          _lrcList.push({
+            lrc,
+            time: time2Seconds
+          })
+        }
+      })
+      console.log(_lrcList)
+      this.setData({
+        lrcList: _lrcList
+      })
+    }
   }
 })
