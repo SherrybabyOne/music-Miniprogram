29eb09e183133da00a248d722ccd1a590b5c5c33
1185491736@qq.com <1185491736@qq.com>
2019-11-04 21:46:24 +0800

    搜索组件封装

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,28 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder: {
+      type: String,
+      value: '请输入关键字'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  externalClasses: [
+    'iconfont', 'icon-sousuo'
+  ],
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
