a212398fdef7e8b2e9b95345d05da2a5babbd9ea
1185491736@qq.com <1185491736@qq.com>
2019-11-05 21:36:31 +0800

    发布页面

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,13 +1,36 @@
 // miniprogram/pages/blog-edit/blog-edit.js
+// 输入文字最大的个数
+const MAX_WORDS_NUM = 140
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 输入的文字个数
+    wordsNum: 0,
+    footerBottom: 0
   },
-
+  onInput(e) {
+    let wordsNum = e.detail.value.length
+    if (wordsNum >= MAX_WORDS_NUM) {
+      wordsNum = `最大字数为${MAX_WORDS_NUM}`
+    }
+    this.setData({
+      wordsNum
+    })
+  },
+  onFocus(e) {
+    // 模拟器键盘高度为0
+    this.setData({
+      footerBottom: e.detail.height
+    })
+  },
+  onBlur() {
+    this.setData({
+      footerBottom: 0
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
