e5be18bd46e9854e1f69103f1f2175460d60f8dd
1185491736@qq.com <1185491736@qq.com>
2019-11-12 22:45:02 +0800

    我的博客功能实现

--- miniprogram/pages/profile-bloghistory/profile-bloghistory.js
+++ miniprogram/pages/profile-bloghistory/profile-bloghistory.js
@@ -1,18 +1,53 @@
 // pages/profile-bloghistory/profile-bloghistory.js
+const MAX_LIMIT = 10
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    blogList: []
   },
 
+  goComment(e) {
+    wx.navigateTo({
+      url: `/pages/blog-comment/blog-comment?blogId=${e.target.dataset.blogid}`,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this._getListByCloudFn()
+  },
+  _getListByCloudFn() {
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.cloud.callFunction({
+      name: 'blog',
+      data: {
+        $url: 'getListByOpenid',
+        start: this.data.blogList.length,
+        count: MAX_LIMIT
+      }
+    }).then(res => {
+      this.setData({
+        blogList: this.data.blogList.concat(res.result)
+      })
+    })
+    wx.hideLoading()
+  },
+  // 小程序查询数据库(测试)
+  _getListByMiniprogram() { 
+    // 小程序端若权限为仅创建者可读写的话，不需要填写openid
+    db.collection('blog').skip(this.data.blogList.length)
+    .limit(MAX_LIMIT)
+    .get()
+    .then(res => {
+      console.log(res, '===')
+    })
   },
 
   /**
@@ -54,13 +89,17 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this._getListByCloudFn()
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function (e) {
+    const blog = e.target.dataset.blog
+    return {
+      title: blog.content,
+      path: `/pages.blog-comment/blog-comment?blogId=${blog._id}`
+    }
   }
 })
