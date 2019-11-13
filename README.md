e8ccc3a9d3e5751d687c2df4452ff003229bcb6b
1185491736@qq.com <1185491736@qq.com>
2019-11-13 15:39:51 +0800

    小程序更新机制

--- README.md
+++ README.md
@@ -248,3 +248,27 @@ page {
   
 }
 ```
+
+### 小程序更新
+```
+checkUpdate() {
+  const updateManager = wx.getUpdateManager()
+  // 检查版本更新
+  updateManager.onCheckForUpdate(res => {
+    if(res.hasUpdate) {
+      updateManager.onUpdateReady(() => {
+        wx.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用',
+          success(res) {
+            if(res.confirm) {
+              updateManager.applyUpdate()
+            }
+          }
+        })
+      })
+    }
+  })
+}
+```
+在`app.js`中调用，每次加载小程序时调用。
--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,8 @@
 //app.js
 App({
   onLaunch: function () {
+
+    this.checkUpdate()
     
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
@@ -36,6 +38,26 @@ App({
       this.globalData.openid = openid
       if (wx.getStorageSync(openid) === '') {
         wx.setStorageSync(openid, [])
+      }
+    })
+  },
+
+  checkUpdate() {
+    const updateManager = wx.getUpdateManager()
+    // 检查版本更新
+    updateManager.onCheckForUpdate(res => {
+      if(res.hasUpdate) {
+        updateManager.onUpdateReady(() => {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用',
+            success(res) {
+              if(res.confirm) {
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
       }
     })
   }
